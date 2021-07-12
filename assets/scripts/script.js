a3d72a7416b72f21dea91f5a361e5859dab2f2c9
Elbin Cenisev <elbincenisev@outlook.com>
2021-07-12 18:41:40 -0400

    Accesses JSON correctly

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,8 +6,8 @@ let radioArtist = radioButtons[0];
 let radioAlbum = radioButtons[1];
 let radioSong = radioButtons[2];
 // When Search Button is clicked..
-searchBtn.addEventListener("click", function () {
-  // e.preventDefault();
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
   // Test statement
   console.log("Inside searchBtn eventListener");
   // Get user input
@@ -36,11 +36,11 @@ function findArtist(artistName) {
   // Fetch API
   corsUrl = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/artist.search?format=json&callback=callback&q_artist=${artistName}&apikey=79b0066e9624b8ba0705fd55e8316a64`;
   url = `https://api.musixmatch.com/ws/1.1/artist.search?format=json&callback=callback&q_artist=${artistName}&apikey=79b0066e9624b8ba0705fd55e8316a64`;
-  let response = fetch(url);
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  let response = fetch(corsUrl);
   fetch(corsUrl)
     .then((response) => response.json())
     .then((data) => console.log(data));
+  // fetch(url)
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data));
 }
